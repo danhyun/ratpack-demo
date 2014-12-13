06ba8644cf4c56c27b0d1c75b69398ed9cef538a
Danny Hyun <hyun.danny+github@gmail.com>
2014-12-13 17:52:27 -0500

    init

---
+++ src/main/java/com/hyunlabs/ratpack/HandlerFactory.java
@@ -0,0 +1,11 @@
+package com.hyunlabs.ratpack;
+
+import ratpack.handling.Handler;
+import ratpack.launch.LaunchConfig;
+
+public class HandlerFactory implements ratpack.launch.HandlerFactory {
+    @Override
+    public Handler create(LaunchConfig launchConfig) throws Exception {
+        return null;
+    }
+}
